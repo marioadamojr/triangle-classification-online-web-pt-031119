9f1f0ec7e755c976a6567518ec51916b7b2faf90
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-14 19:39:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,9 +5,16 @@ class Triangle
     @side_a=side_a
     @side_b=side_b
     @side_c=side_c
+    @sides = [@side_a, @side_b, @side_c]
   end
 
   def kind
+    if @sides.include?(0) == true
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
     if @side_a == @side_b && @side_a == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
@@ -19,6 +26,7 @@ class Triangle
 
   class TriangleError < StandardError
     def message
+      "uh oh"
     end
   end
 end

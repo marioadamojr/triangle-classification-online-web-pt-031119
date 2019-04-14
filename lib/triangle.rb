8aaa22fb3e35fab879df5e7d32bc34be568aeb1c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-14 20:04:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       end
     end
 
-    if @side_a > (@side_b + @side_c) || @side_b > (@side_a + @side_c) || @side_c > (@side_b + @side_a)
+    if @side_a >= (@side_b + @side_c) || @side_b >= (@side_a + @side_c) || @side_c >= (@side_b + @side_a)
       begin
         raise TriangleError
       end
